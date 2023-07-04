45086836e0fda86da6603dbb0e7cb9c0fb99f266
Giovany Oliva <giovanyolivacarrasco@gmail.com>
2023-07-04 13:01:56 -0400

    actualizacion codigo sql para ejecucion mas sencilla

--- sql_ddl.sql
+++ sql_ddl.sql
@@ -1,15 +1,24 @@
+--EJECUTAR ESTOS COMANDOS DESDE SQL Shell (psql)
+--(copy and paste)
 
+--CREAR BASE DE DATOS
+CREATE DATABASE tareas;
 
+--CONECCION A BASE DE DATOS 'tareas'
+\c tareas
+
+--CREAR TABLA USUARIOS
+CREATE TABLE usuarios (
+	id serial primary key,
+	nombre varchar (255),
+	correo varchar(255)
+);
+
+--CREAR TABLA TAREAS
 CREATE TABLE tareas (
 	id serial primary key,
 	titulo varchar(255),
 	descripcion varchar (255),
 	completado boolean,
 	usuario_id int references usuarios (id)
-);
-
-CREATE TABLE usuarios (
-	id serial primary key,
-	nombre varchar (255),
-	correo varchar(255)
 );
